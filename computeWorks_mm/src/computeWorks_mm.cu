b5993f40001e058b65ad1ae48a32dd4693b0181c
nicelyma <matthew.a.nicely.civ@mail.mil>
2019-06-10 11:34:55 -0400

    Updated GPU timers

--- computeWorks_mm/src/computeWorks_mm.cu
+++ computeWorks_mm/src/computeWorks_mm.cu
@@ -1,60 +1,53 @@
 #include <chrono>		// std::chrono
 #include <cmath> 		// std::sqrt, std::fabs
 #include <cstdio>		// std::printf
-#include <cstdlib>		// std::atoi, std::free
-#include <functional>	// std::function
+#include <cstdlib>		// std::atoi
 #include <stdexcept>	// std::runtime_error
-#include <omp.h>		// OpenMP
+#include <omp.h>		// OpenMP - PGI
+#include <cblas.h>		// OpenBLAS - PGI
 #include <cublas_v2.h>	// cuBLAS
 
 auto constexpr tPB = 16;
 
-auto benchmark = [](auto fcnPtr, int const & loops) {
+auto startTimer( ) {
+	return (std::chrono::high_resolution_clock::now());
+}
 
-	// Warmup run
-		fcnPtr();
+auto stopTimer( ) {
+	return (std::chrono::high_resolution_clock::now());
+}
 
-		// Host timing variables
-		std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
-		std::chrono::duration<double, std::milli> elapsed_ms;
+auto startGPUTimer( ) {
+	cudaEvent_t startEvent = nullptr;
+	cudaEventCreate( &startEvent, cudaEventBlockingSync );
+	cudaEventRecord( startEvent );
 
-		start = std::chrono::high_resolution_clock::now();
+	return (startEvent);
+}
 
-		for ( int l = 0; l < loops; l++ ) {
-			fcnPtr();
-		}
+auto stopGPUTimer( ) {
+	cudaEvent_t stopEvent = nullptr;
+	cudaEventCreate( &stopEvent, cudaEventBlockingSync );
+	cudaEventRecord( stopEvent );
+	cudaEventSynchronize( stopEvent );
 
-		end = std::chrono::high_resolution_clock::now();
-		elapsed_ms = end - start;
-		std::printf( "%0.2f ms\n", elapsed_ms.count() / loops );
+	return (stopEvent);
+}
 
-	};
+template<typename T>
+void printTime( T start, T stop, int const & loops ) {
+	std::chrono::duration<double, std::milli> elapsed_ms;
 
-auto benchmarkGPU = [](auto fcnPtr, int const & loops) {
+	elapsed_ms = stop - start;
+	std::printf( "%0.2f ms\n", elapsed_ms.count() / loops );
+}
 
-	// Warmup run
-		fcnPtr();
-
-		// Create timers
-		cudaEvent_t startEvent = nullptr, stopEvent = nullptr;
-
-		cudaEventCreate( &startEvent, cudaEventBlockingSync );
-		cudaEventCreate( &stopEvent, cudaEventBlockingSync );
-
-		cudaEventRecord( startEvent );
-
-		for ( int l = 0; l < loops; l++ ) {
-			fcnPtr();
-		}
-
-		cudaEventRecord( stopEvent );
-		cudaEventSynchronize( stopEvent );
-		float elapsed_ms;
-		cudaEventElapsedTime( &elapsed_ms, startEvent, stopEvent );
-
-		std::printf( "%0.2f ms\n", elapsed_ms / loops );
-
-	};
+template<typename T>
+void printGPUTime( T startEvent, T stopEvent, int const & loops ) {
+	float elapsed_ms;
+	cudaEventElapsedTime( &elapsed_ms, startEvent, stopEvent );
+	std::printf( "%0.2f ms\n", elapsed_ms / loops );
+}
 
 void print( int const & n, float const * C ) {
 	for ( int i = 0; i < n; i++ ) {
@@ -79,13 +72,11 @@ void verify( int const & n, float const * C_ref, float const * C_test ) {
 		ref_norm += C_test[i] * C_test[i];
 	}
 
-	error_norm = static_cast<float>( std::sqrt(
-			static_cast<double>( error_norm ) ) );
-	ref_norm =
-			static_cast<float>( std::sqrt( static_cast<double>( ref_norm ) ) );
+	error_norm = static_cast<float>( std::sqrt( static_cast<double>( error_norm ) ) );
+	ref_norm = static_cast<float>( std::sqrt( static_cast<double>( ref_norm ) ) );
 
 	if ( std::fabs( ref_norm ) < 1e-7 )
-		throw std::runtime_error( "!!!! reference norm is 0\n" );
+		std::printf( "Reference norm is 0.\t" );
 
 	if ( error_norm / ref_norm < 1e-5f )
 		std::printf( "Test passed.\n" );
@@ -99,20 +90,30 @@ void normalC(
 		float const * A,
 		float const * B,
 		float const & beta,
-		float * C ) {
+		float * C,
+		int const & loops ) {
 
-	for ( int i = 0; i < n; ++i ) {
-		for ( int j = 0; j < n; ++j ) {
+	auto start = startTimer();
 
-			float prod = 0.0f;
+	for ( int l = 0; l < loops; l++ ) {
+		for ( int i = 0; i < n; ++i ) {
+			for ( int j = 0; j < n; ++j ) {
 
-			for ( int k = 0; k < n; ++k ) {
-				prod += A[k * n + i] * B[j * n + k];
-			}
+				float prod = 0.0f;
 
-			C[j * n + i] = alpha * prod + beta * C[j * n + i];
-		}
-	}
+				for ( int k = 0; k < n; ++k ) {
+					prod += A[k * n + i] * B[j * n + k];
+				}
+
+				C[j * n + i] = alpha * prod + beta * C[j * n + i];
+			} // j
+		} // i
+	} // loops
+
+	auto end = stopTimer();
+
+	printTime( start, end, loops );
+
 }
 
 void openMP(
@@ -121,25 +122,56 @@ void openMP(
 		float const * A,
 		float const * B,
 		float const & beta,
-		float * C ) {
+		float * C,
+		int const & loops ) {
 
 	// Request number of threads at runtime
-	omp_set_num_threads( 4 );
+	omp_set_num_threads( 6 );
 
-	// Create parallel region and worksharing
-#pragma omp parallel for shared(A, B, C, n)
-	for ( int i = 0; i < n; ++i ) {
-		for ( int j = 0; j < n; ++j ) {
+	auto start = startTimer();
 
-			float prod = 0.0f;
+	for ( int l = 0; l < loops; l++ ) {
 
-			for ( int k = 0; k < n; ++k ) {
-				prod += A[k * n + i] * B[j * n + k];
+		// Create parallel region and worksharing
+#pragma omp parallel for shared(A, B, C, n) schedule(static)
+		for ( int i = 0; i < n; ++i ) {
+			for ( int j = 0; j < n; ++j ) {
+				float prod = 0.0f;
+				for ( int k = 0; k < n; ++k ) {
+					prod += A[k * n + i] * B[j * n + k];
+				}
+
+				C[j * n + i] = alpha * prod + beta * C[j * n + i];
 			}
-
-			C[j * n + i] = alpha * prod + beta * C[j * n + i];
 		}
 	}
+
+	auto end = stopTimer();
+
+	printTime( start, end, loops );
+}
+
+void blas(
+		int const & n,
+		float const & alpha,
+		float const * A,
+		float const * B,
+		float const & beta,
+		float * C,
+		int const & loops ) {
+
+	openblas_set_num_threads( 6 );
+
+	auto start = startTimer();
+
+	for ( int l = 0; l < loops; l++ ) {
+
+		cblas_sgemm( CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, alpha, A, n, B, n, beta, C,
+				n );
+	}
+	auto end = stopTimer();
+
+	printTime( start, end, loops );
 }
 
 void openACC(
@@ -148,21 +180,24 @@ void openACC(
 		float const * A,
 		float const * B,
 		float const beta,
-		float * C ) {
+		float * C,
+		int const & loops ) {
 
+	for ( int l = 0; l < loops; l++ ) {
 
 #pragma acc kernels copyin(A[0:(n*n)], B[0:(n*n)]) copyout(C[0:(n*n)])
 #pragma acc loop independent
-	for ( int i = 0; i < n; ++i ) {
-		#pragma acc loop independent
-		for ( int j = 0; j < n; ++j ) {
-			float prod = 0.0f;
-			#pragma acc loop independent reduction(+:prod)
-			for ( int k = 0; k < n; ++k ) {
-				prod += A[k * n + i] * B[j * n + k];
-			}
+		for ( int i = 0; i < n; ++i ) {
+#pragma acc loop independent
+			for ( int j = 0; j < n; ++j ) {
+				float prod = 0.0f;
+#pragma acc loop independent reduction(+:prod)
+				for ( int k = 0; k < n; ++k ) {
+					prod += A[k * n + i] * B[j * n + k];
+				}
 
-			C[j * n + i] = alpha * prod + beta * C[j * n + i];
+				C[j * n + i] = alpha * prod + beta * C[j * n + i];
+			}
 		}
 	}
 }
@@ -173,10 +208,13 @@ void cublas(
 		float const * A,
 		float const * B,
 		float const & beta,
-		float * C ) {
+		float * C,
+		int const & loops ) {
 
-	// Declare device result pointers
+	// Declare device pointers and cublas handle
 	float *d_A, *d_B, *d_C;
+	cublasHandle_t handle;
+	cublasCreate( &handle );
 
 	// Allocate memory on device
 	cudaMalloc( (void **) &d_A, sizeof(float) * n * n );
@@ -187,18 +225,23 @@ void cublas(
 	cudaMemcpy( d_A, A, sizeof(float) * n * n, cudaMemcpyHostToDevice );
 	cudaMemcpy( d_B, B, sizeof(float) * n * n, cudaMemcpyHostToDevice );
 
-	cublasHandle_t handle;
-	cublasCreate( &handle );
-
-	cublasSgemm( handle, CUBLAS_OP_N, CUBLAS_OP_N, n, n, n, &alpha, d_A, n, d_B,
-			n, &beta, d_C, n );
+	auto startEvent = startGPUTimer();
+	for ( int l = 0; l < loops; l++ )
+		cublasSgemm( handle, CUBLAS_OP_N, CUBLAS_OP_N, n, n, n, &alpha, d_A, n, d_B, n, &beta, d_C,
+				n );
+	auto stopEvent = stopGPUTimer();
 
 	cudaDeviceSynchronize();
 
 	// Copy results from device to host
 	cudaMemcpy( C, d_C, sizeof(float) * n * n, cudaMemcpyDeviceToHost );
 
+	cudaFree( d_A );
+	cudaFree( d_B );
+	cudaFree( d_C );
 	cublasDestroy( handle );
+
+	printGPUTime(startEvent, stopEvent, loops);
 }
 
 __global__ void cudaKernel(
@@ -227,7 +270,8 @@ void cuda(
 		float const * A,
 		float const * B,
 		float const & beta,
-		float * C ) {
+		float * C,
+		int const & loops ) {
 
 	// Declare device result pointers
 	float *d_A, *d_B, *d_C;
@@ -245,11 +289,21 @@ void cuda(
 	dim3 blocksPerGrid( ( n + tPB - 1 ) / tPB, ( n + tPB - 1 ) / tPB );
 	dim3 threadsPerBlock( tPB, tPB );
 
-	cudaKernel<<<blocksPerGrid, threadsPerBlock>>>(n, d_A, d_B, d_C);
+	auto startEvent = startGPUTimer();
+	for ( int l = 0; l < loops; l++ )
+		cudaKernel<<<blocksPerGrid, threadsPerBlock>>>(n, d_A, d_B, d_C);
+	auto stopEvent = stopGPUTimer();
+
 	cudaDeviceSynchronize();
 
 	// Copy results from device to host
 	cudaMemcpy( C, d_C, sizeof(float) * n * n, cudaMemcpyDeviceToHost );
+
+	cudaFree( d_A );
+	cudaFree( d_B );
+	cudaFree( d_C );
+
+	printGPUTime(startEvent, stopEvent, loops);
 }
 
 int main( int argc, char** argv ) {
@@ -257,11 +311,11 @@ int main( int argc, char** argv ) {
 	int n;
 	if ( argc < 2 ) {
 		n = 1024;
-		printf("No input given.\n");
-		printf("Running with N = %d\n\n", n);
+		printf( "No input given.\n" );
+		printf( "Running with N = %d\n\n", n );
 	} else {
 		n = std::atoi( argv[1] );
-		printf("Running with N = %d\n\n", n);
+		printf( "Running with N = %d\n\n", n );
 	}
 
 	float alpha = 1.0f;
@@ -272,6 +326,7 @@ int main( int argc, char** argv ) {
 	float *h_B = new float[sizeof(float) * n * n];
 	float *h_C = new float[sizeof(float) * n * n];
 	float *h_C_mp = new float[sizeof(float) * n * n];
+	float *h_C_blas = new float[sizeof(float) * n * n];
 	float *h_C_acc = new float[sizeof(float) * n * n];
 	float *h_C_cublas = new float[sizeof(float) * n * n];
 	float *h_C_cuda = new float[sizeof(float) * n * n];
@@ -284,27 +339,32 @@ int main( int argc, char** argv ) {
 
 	// Benchmark normal C matrix multiplication
 	printf( "Running Normal C: " );
-	benchmark( [&] {normalC( n, alpha, h_A, h_B, beta, h_C );}, 2 );
+	normalC( n, alpha, h_A, h_B, beta, h_C, 2 );
 
 	// Benchmark and verify OpenMP matrix multiplication
 	printf( "Running OpenMP: " );
-	benchmark( [&] {openMP( n, alpha, h_A, h_B, beta, h_C_mp );}, 10 );
+	openMP( n, alpha, h_A, h_B, beta, h_C_mp, 5 );
 	verify( n, h_C, h_C_mp );
 
-	// Benchmark and verify OpenACC matrix multiplication
-	printf( "Running OpenACC: " );
-	benchmark( [&] {openACC( n, alpha, h_A, h_B, beta, h_C_acc );}, 10 );
-	verify( n, h_C, h_C_acc );
+	// Benchmark and verify OpenMP matrix multiplication
+	printf( "Running BLAS: " );
+	blas( n, alpha, h_A, h_B, beta, h_C_blas, 500 );
+	verify( n, h_C, h_C_blas );
 
 	// Benchmark and verify CUBLAS matrix multiplication
 	printf( "Running CUBLAS: " );
-	benchmarkGPU( [&] {cublas( n, alpha, h_A, h_B, beta, h_C_cublas );}, 10 );
+	cublas( n, alpha, h_A, h_B, beta, h_C_cublas, 500 );
 	verify( n, h_C, h_C_cublas );
 
 	// Benchmark and verify CUDA matrix multiplication
 	printf( "Running CUDA: " );
-	benchmarkGPU( [&] {cuda( n, alpha, h_A, h_B, beta, h_C_cuda );}, 10 );
+	cuda( n, alpha, h_A, h_B, beta, h_C_cuda, 500 );
 	verify( n, h_C, h_C_cuda );
+
+	// Benchmark and verify OpenACC matrix multiplication
+	printf( "Running OpenACC: " );
+	openACC( n, alpha, h_A, h_B, beta, h_C_acc, 500 );
+	verify( n, h_C, h_C_acc );
 
 	// Memory clean up
 	delete ( h_A );
